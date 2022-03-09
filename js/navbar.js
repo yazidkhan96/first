a60a6b5da86d30730301406cf508a83465a3f0d3
yazidkhan96 <51899334+yazidkhan96@users.noreply.github.com>
2022-03-09 13:53:16 +0700

    Delete js directory

--- js/navbar.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.querySelector('.menu-toggle input');
-const nav = document.querySelector('.nav-menu ul');
-
-menuToggle.addEventListener('click', () => {
-    nav.classList.toggle('slide');
-})
